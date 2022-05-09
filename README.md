cccd0f3d1b9207816a112b1efd800ffd37f90572
Leonardo Barboza Borges <54484227+leonbborges@users.noreply.github.com>
2022-05-09 00:22:18 -0300

    [alteracao01][09/05/2022]
    alterações em algumas partes do projeto principal e no README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # PerceptronTopicos1
  projeto de perceptron para cadeira de topicos 1
+
+#### Alunos: Leonardo Barboza Borges e Luciano Marques Brito Reis.
+
+## Enunciado:
+Pela análise de um processo de destilação fracionada de petróleo observou-se que determinado óleo
+poderia ser classificado em duas classes de pureza {P1 e P2} a partir da medição de três grandezas 
+{x1, x2 e x3}, que representam algumas de suas propriedades físico-químicas. A equipe de
+engenheiros e cientistas pretende usar urna rede Perceptron para executar a classificação automática
+das duas classes.
+Assim, baseado nas informações coletadas do processo, formou-se o conjunto de treinamento
+apresentado no apêndice I, tomando por convenção o valor -1 para óleo pertencente à classe P1 e o
+valor 1 para óleo pertencente à classe P2. Para tanto, o neurônio constituinte do Perceptron terá
+então três entradas e uma saída conforme ilustrado na figura abaixo. 
+
+![Perceptron Structure](imagens/Perceptron_structure.png)<br/>
+**Figura 1: Estrutura básica de um Perceptron**
+
+## oque foi proposto?
+em suma oque foi proposto era que usando essa base de dados disponível nesse [dataset](content/Dataset.txt),
+disponibilizado pelo professor. nosso trabalho e usar a logica do perceptron como foi mostrado o problema no
+Enunciado acima.
+
+### funcionamento:
+o código possui tais elementos como variáveis:
+* entradas
+* saídas
+* taxa de aprendizagem
+* épocas
+* limiar <br/>
+
+ sendo o processo começa com a leitura do arquivo aonde estra o dataset em arquivo csv
+[datasetCSV](./content/dataset.csv) aonde as amostras são convertidas para float para
+poderem ser usadas na  classe percptron aonde a logica do perceptron e aplicada
+depois os dados são copiados para serem usados mais tarde.<br/><br/>
+apos isso ele são jogados na classe perceptron aonde em aplicado toda logística dos
+cálculos que são feitos. a imagem a seguir ilustra os cálculos que são feitos:<br/><br/>
+![esqução do perceptron](imagens/equacao_perceptron.png)
+**Figura 2: equação usada para logica de Perceptron** 
+<br/><br/>
+essa logica e aplicada como a função do perceptron trabalha seguindo essa logica de 
+calculo, nisso ela faz o treinamento que e o ajuste de pesos para que posteriormente
+na função de teste podemos diferenciar se um óleo pertencer a classe P1
+ou se ele pertence a classe P2
+
--- perceptron.py
+++ perceptron.py
@@ -7,18 +7,16 @@ class Perceptron:
 
     def __init__(self, amostras, saidas, taxa_aprendizado=0.1, epocas=1000, limiar=-1):
 
-        self.amostras = amostras  # todas as amostras
-        self.saidas = saidas  # saídas respectivas de cada amostra
-        # taxa de aprendizado (entre 0 e 1)
-        self.taxa_aprendizado = taxa_aprendizado
-        self.epocas = epocas  # número de épocas
-        self.limiar = limiar  # limiar
-        self.num_amostras = len(amostras)  # quantidade de amostras
-        # quantidade de elementos por amostra
+        self.amostras = amostras
+        self.saidas = saidas
+        self.taxa_aprendizado = taxa_aprendizado    # taxa de aprendizado (entre 0 e 1)
+        self.epocas = epocas
+        self.limiar = limiar
+        self.num_amostras = len(amostras)
         self.num_amostra = len(amostras[0])
-        self.pesos = []  # vetor de pesos
+        self.pesos = []
 
-    # função para treinar a rede
+    # função que recebe as amostras de oleo e as treinas
 
     def treinar(self):
 
@@ -30,10 +28,8 @@ class Perceptron:
         for i in range(self.num_amostra):
             self.pesos.append(random.random())
 
-        # insere o limiar no vetor de pesos
         self.pesos.insert(0, self.limiar)
 
-        # inicia o contador de epocas
         num_epocas = 0
 
         while True:
@@ -47,50 +43,40 @@ class Perceptron:
                 for j in range(self.num_amostra + 1):
                     u += self.pesos[j] * self.amostras[i][j]
 
-                # obtém a saída da rede utilizando a função de ativação
                 y = self.sinal(u)
-
-                # verifica se a saída da rede é diferente da saída desejada
                 if y != self.saidas[i]:
 
-                    # calcula o erro: subtração entre a saída desejada e a saída da rede
                     erro_aux = self.saidas[i] - y
 
-                    # faz o ajuste dos pesos para cada elemento da amostra
+                    # faz o ajuste dos pesos
                     for j in range(self.num_amostra + 1):
                         self.pesos[j] = self.pesos[j] + \
                             self.taxa_aprendizado * \
                             erro_aux * self.amostras[i][j]
 
-                    erro = True  # ainda existe erro
+                    erro = True
 
-            # incrementa o número de épocas
+            # aumenta numero de epocas
             num_epocas += 1
 
-            # critério de parada é pelo número de épocas ou se não existir erro
+            # se o numero de epocas bater ou não haver erro
             if num_epocas > self.epocas or not erro:
                 break
 
     # função utilizada para testar a rede
-    # recebe uma amostra a ser classificada e os nomes das classes
-    # utiliza a função sinal, se é -1 então é classe1, senão é classe2
+    # ela classifica as amostra tanto em P1 como P2
+    # sendo p1 valor -1 e p2 1
 
     def testar(self, amostra, classe1, classe2):
 
-        # insere o -1
         amostra.insert(0, -1)
-
-        # utiliza o vetor de pesos que foi ajustado na fase de treinamento
         u = 0
         for i in range(self.num_amostra + 1):
             u += self.pesos[i] * amostra[i]
 
-        # calcula a saída da rede
         y = self.sinal(u)
-
-        # verifica a qual classe pertence
         
-        print(f'A amostra pertence a classe A') if y == -1 else print(f'A amostra pertence a classe B')
+        print(f'A amostra de óleo pertence a classe de pureza P1') if y == -1 else print(f'A amostra de óleo pertence a classe de pureza P2')
 
     # função de ativação: degrau bipolar (sinal)
 
@@ -98,9 +84,9 @@ class Perceptron:
         return 1 if u >= 0 else -1
 
 
-print('\n A AMostra pertence ao grupo :A ou B?\n')
+print('\n A Amostra de óleo pertence a qual classe de pureza?P1 ou P2?\n')
 
-# amostras: um total de 4 amostras
+# começo do da implementação com as amostras e saidas
 amostras = []
 saidas = []
 
@@ -110,18 +96,13 @@ with open('./content/dataset.csv', 'r') as dados:
         amostras.append([float(dado['x1']), float(dado['x2']), float(dado['x3'])])
         saidas.append(float(dado['d']))
 
-# saídas desejadas de cada amostra
-
-# conjunto de amostras de testes
 testes = copy.deepcopy(amostras)
 
-# cria uma rede Perceptron
-rede = Perceptron(amostras=amostras, saidas=saidas,
+# aqui aonde e rede do percptron e criada com entradas, saidas e a taxa de aprendizagem
+redePerceptron = Perceptron(amostras=amostras, saidas=saidas,
                   taxa_aprendizado=0.1, epocas=1000)
 
-# treina a rede
-rede.treinar()
+redePerceptron.treinar()
 
-# testando a rede
 for teste in testes:
-    rede.testar(teste, 'A', 'B')
+    redePerceptron.testar(teste, 'P1', 'P2')
